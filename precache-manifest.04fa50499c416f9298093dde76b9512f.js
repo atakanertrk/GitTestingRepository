e210fd4fe3ea725408fa3428881e94cb49655770
atakan erturk <atakanertrk@hotmail.com>
2020-05-03 01:47:17 +0300

    asdas

---
+++ precache-manifest.04fa50499c416f9298093dde76b9512f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd65042e5b28434237a75f7d60c7e08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ad6c53bab021e0a54df",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "c1c3e9981ee8c78b3984",
+    "url": "/static/css/main.7b5c3b7b.chunk.css"
+  },
+  {
+    "revision": "7ad6c53bab021e0a54df",
+    "url": "/static/js/2.479e6b44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.479e6b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c3e9981ee8c78b3984",
+    "url": "/static/js/main.4e7a8275.chunk.js"
+  },
+  {
+    "revision": "46b2ab5944883490d30d",
+    "url": "/static/js/runtime-main.501fdd23.js"
+  }
+]);
